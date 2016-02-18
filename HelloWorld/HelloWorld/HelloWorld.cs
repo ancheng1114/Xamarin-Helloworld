abd1277dc619994400f8f2326a7642c10d4f6699
ancheng1114 <acu11988@gmail.com>
2016-02-18 10:35:14 +0800

    init commit

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+
+			FormattedString formattedString = new FormattedString();
+			formattedString.Spans.Add(new Span
+				{
+					Text = "I "
+				});
+			formattedString.Spans.Add(new Span
+				{
+					Text = "love",
+					FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+					FontAttributes = FontAttributes.Bold
+				});
+			formattedString.Spans.Add(new Span
+				{
+					Text = " Xamarin.Forms!"
+				});
+			
+			MainPage = new ContentPage {
+				Content = new Label {
+					HorizontalOptions = LayoutOptions.Center,
+					VerticalOptions = LayoutOptions.Center,
+					FormattedText = formattedString,
+					//Text = "Welcome to Xamarin Forms!",
+					//BackgroundColor = Color.Yellow,
+					//FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+					//FontAttributes = FontAttributes.Bold | FontAttributes.Italic,
+					//TextColor = Color.Blue
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
